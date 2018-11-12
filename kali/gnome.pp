b1959e0a8d8333fa4fcab9f063cd50401972bc3a
deanturpin <kali@brightcoin.uk>
2018-11-12 12:37:05 +0000

    Add test to check settings have been applied

--- kali/gnome.pp
+++ kali/gnome.pp
@@ -1,13 +1,21 @@
-$_gnome = "/usr/bin/gsettings set"
+$_gnome = "/usr/bin/gsettings"
+$_set = "${_gnome} set"
+$_get = "${_gnome} get"
 
+$_touchpad = "org.gnome.desktop.peripherals.touchpad tap-to-click"
 exec { touchpad:
-	command => "${_gnome} org.gnome.desktop.peripherals.touchpad tap-to-click true"
+  command => "${_set} ${_touchpad} true",
+  unless => "/usr/bin/test \$(${_get} ${_touchpad}) = true"
 }
 
+$_idle = "org.gnome.desktop.session idle-delay"
 exec { idle-delay:
-	command => "${_gnome} org.gnome.desktop.session idle-delay 3600"
+  command => "${_set} ${_idle} 3600",
+  onlyif => "/usr/bin/test \$(${_get} ${_idle}) -ne 3600"
 }
 
+$_ambient = "org.gnome.settings-daemon.plugins.power ambient-enabled"
 exec { ambient-enabled:
-	command => "${_gnome} org.gnome.settings-daemon.plugins.power ambient-enabled false"
+  command => "${_set} ${_ambient} false",
+  onlyif => "/usr/bin/test \$(${_get} ${_ambient}) -ne false"
 }
