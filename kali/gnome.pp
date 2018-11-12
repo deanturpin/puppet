d8606ca4ea59ff14f1aa11cdc24c32f95e4badd6
deanturpin <kali@brightcoin.uk>
2018-11-12 13:45:51 +0000

    Use a path

--- kali/gnome.pp
+++ kali/gnome.pp
@@ -1,21 +1,23 @@
-$_gnome = "/usr/bin/gsettings"
-$_set = "${_gnome} set"
-$_get = "${_gnome} get"
+$_set = "gsettings set"
+$_get = "gsettings get"
 
 $_touchpad = "org.gnome.desktop.peripherals.touchpad tap-to-click"
 exec { touchpad:
   command => "${_set} ${_touchpad} true",
-  unless => "/usr/bin/test \$(${_get} ${_touchpad}) = true"
+  unless => "test \$(${_get} ${_touchpad}) = true",
+  path => "/usr/bin/"
 }
 
 $_idle = "org.gnome.desktop.session idle-delay"
 exec { idle-delay:
   command => "${_set} ${_idle} 3600",
-  onlyif => "/usr/bin/test \$(${_get} ${_idle}) -ne 3600"
+  onlyif => "test \$(${_get} ${_idle}) -ne 3600",
+  path => "/usr/bin/"
 }
 
 $_ambient = "org.gnome.settings-daemon.plugins.power ambient-enabled"
 exec { ambient-enabled:
   command => "${_set} ${_ambient} false",
-  onlyif => "/usr/bin/test \$(${_get} ${_ambient}) -ne false"
+  onlyif => "test \$(${_get} ${_ambient}) -ne false",
+  path => "/usr/bin/"
 }
