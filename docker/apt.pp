2116ecab825ad7afad73bc18b97278f1025c9a82
deanturpin <kali@brightcoin.uk>
2018-11-14 19:28:55 +0000

    Resource chain

--- docker/apt.pp
+++ docker/apt.pp
@@ -1,9 +1,9 @@
-package { vim: }
+include apt
 
 exec { upgrade:
 	command => "/usr/bin/apt upgrade || true"
 }
 
-exec { full-upgrade:
+-> exec { full-upgrade:
 	command => "/usr/bin/apt full-upgrade || true"
 }
